42c18c70e41d2dfec329ee59c62e6a2cb7db65f2
Sorra <sorra@thewizardscode.com>
2025-12-21 02:32:21 -0800

    Revert "wf-udv: add waif ask command (MVP) (#19)" (#20)
    This reverts commit 65c4b052be584a642732eccf48808b5912d5ec66.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { realpathSync } from 'node:fs';
 import { fileURLToPath } from 'node:url';
 import { Command } from 'commander';
 import { createPrdCommand } from './commands/prd.js';
-import { createAskCommand } from './commands/ask.js';
 import { createNextCommand } from './commands/next.js';
 import { createRecentCommand } from './commands/recent.js';
 import { createInProgressCommand } from './commands/inProgress.js';
@@ -28,7 +27,6 @@ export async function run(argv = process.argv.slice(2)): Promise<number> {
     .showHelpAfterError();
 
   program.addCommand(createPrdCommand());
-  program.addCommand(createAskCommand());
   program.addCommand(createNextCommand());
   program.addCommand(createRecentCommand());
   program.addCommand(createInProgressCommand());
--- src/types/opencode.d.ts
+++
@@ -1,1 +0,0 @@
-declare module '@opencode-ai/sdk';
